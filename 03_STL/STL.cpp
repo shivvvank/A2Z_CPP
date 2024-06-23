e04bf0b273967df4d9f35f31a497ee3e08ae9fad
Shivvvank <shivvvank@gmail.com>
2024-06-23 15:38:28 +0530

    Changed local git config

---
+++ 03_STL/STL.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+ 
+void solve()
+{
+    
+}
+ 
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    solve();
+    return 0;
+}
